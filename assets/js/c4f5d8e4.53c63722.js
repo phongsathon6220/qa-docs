5dee7544ee45aba7f8446eb60673161b675b2782
phongsathon6220 <phongsathon6220@seniorsoft.co.th>
2025-07-23 11:27:14 +0700

    Deploy website - based on 8ee1cdbaca3fe16e66c2a487886681f27bf1bd18

---
+++ assets/js/c4f5d8e4.53c63722.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqa_docs=self.webpackChunkqa_docs||[]).push([[2634],{192:(s,c,e)=>{e.r(c),e.d(c,{default:()=>u});e(6540);var r=e(6347),t=e(4848);function u(){return(0,t.jsx)(r.rd,{to:"/docs/intro"})}}}]);
